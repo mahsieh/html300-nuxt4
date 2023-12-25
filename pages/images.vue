32a451dbea1f3ad5d89e45b2f3c0f1ad6114c997
mahsieh <michaelhsieh.alt@gmail.com>
2023-12-25 00:03:57 -0800

    pulled some previous code into project

---
+++ pages/images.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="images">
+        <h1>This is an images page</h1>
+        <!--iterate through images object and display each image individually-->
+        <div v-for="image in imageList">
+            <!--actually use the image component-->
+            <ImageComponent :src="image.src" :caption="image.caption" :title="image.title" />
+        </div>
+    </div>
+</template>
+
+<script>
+// import the image component we created for use
+import ImageComponent from '../components/ImageComponent.vue'
+
+export default {
+    name: "ImagesView",
+    components: {
+        ImageComponent,
+    },
+    data: () => {
+        return {
+            imageList: [
+                {
+                    src: "../static/travel.jpg",
+                    caption: "Tooltipped image depicting air travel to various locations",
+                    title: ''
+                },
+                {
+                    src: "../static/urban_vacation.jpg",
+                    caption: "Tooltipped image of Paris near Eiffel Tower",
+                    title: ''
+                },
+                {
+                    src: "../static/tropical_vacation.jpg",
+                    caption: "Tooltipped image of a tropical island with pier",
+                    title: ''
+                }
+            ],
+        }
+    }
+}
+</script>
+  
+<style scope>
+@media (min-width: 1024px) {
+    .about {
+        min-height: 100vh;
+        display: flex;
+        align-items: center;
+    }
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,17 @@
 <template>
-  <Tutorial/>
+  <header>
+    <div class="wrapper">
+      <HelloWorld msg="You did the web development thing..!" />
+
+      <!--added NuxtLinks for additional pages-->
+      <nav>
+        <NuxtLink to="/">Home</NuxtLink>
+        <NuxtLink to="/about">About</NuxtLink>
+        <NuxtLink to="/accordion">Accordion</NuxtLink>
+        <NuxtLink to="/images">Images</NuxtLink>
+      </nav>
+    </div>
+  </header>
 </template>
 
 <script>
